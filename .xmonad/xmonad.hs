c8cb7e2007ed7f34515bf311fe552a2a5f643a4c
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2020-10-22 12:40:56 -0300

    create branch for desktop xmonad configuration dotfiles

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,10 @@
---import XMonad.Hooks.EwmhDesktops
 import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Spiral
@@ -13,13 +13,14 @@ import XMonad.Util.Paste
 import XMonad.Util.Run (spawnPipe)
 
 modKey :: KeyMask
---modKey = mod4Mask  -- super
-modKey = mod1Mask  -- left alt
+modKey = mod4Mask  -- super
+--modKey = mod1Mask  -- left alt
 
 myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "Zenity"                       --> doFloat
-    , className =? "Steam"                        --> doShift "2"
+    , className =? "Steam"                        --> doShift "8"
+    , className =? "Spotify Premium"              --> doShift "7"
     , wmRole    =? "gimp-message-dialog"          --> doFloat
     , wmRole    =? "gimp-toolbox-color-dialog"    --> doFloat
     ]
@@ -32,34 +33,28 @@ myLayouts = (gaps 10 $ Tall 1 (3/100) (1/2)) ||| (noBorders Full)
 xK_AudioLower     = 0x1008FF11
 xK_AudioMute      = 0x1008FF12
 xK_AudioRaise     = 0x1008FF13
-xK_BrightnessUp   = 0x1008FF02
-xK_BrightnessDown = 0x1008FF03
-xK_KbdLightOn     = 0x1008FF04
-xK_KbdLightOff    = 0x1008FF05
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad . docks $ def
-        { borderWidth        = 2
+        { borderWidth        = 3
         , modMask            = modKey
-        , focusedBorderColor = "#22D05F"
+        , focusedBorderColor = "#fb27ff"
         , terminal           = "kitty"
         --, focusFollowsMouse  = False
         --, clickJustFocuses   = False
         , layoutHook         = avoidStruts $ myLayouts
         , manageHook         = myManageHook <+> manageHook def
+        , handleEventHook    = fullscreenEventHook
         , logHook            = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc
             , ppTitle = xmobarColor "green" "" . shorten 50
             }
-        }
-        `additionalKeys`
+        } `additionalKeys`
         [ ((0, xK_AudioLower),     spawn "volume dec")
         , ((0, xK_AudioRaise),     spawn "volume inc")
         , ((0, xK_AudioMute),      spawn "volume toggle")
-        , ((0, xK_BrightnessUp),   spawn "brightness inc")
-        , ((0, xK_BrightnessDown), spawn "brightness dec")
-        , ((modKey, xK_space),     spawn "dmenu_run")
+        , ((modKey, xK_space),     spawn "dmenu_run -b")
         , ((modKey, xK_f),         sendMessage NextLayout)
         , ((modKey, xK_b),         sendMessage ToggleStruts)
         ]
