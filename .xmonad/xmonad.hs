450f4a023f1ded3910010c90dbfec335c3f3b2aa
Kevin Liu Rodrigues <kkevin.liu@protonmail.com>
2021-03-06 20:23:17 -0300

    music with modkey, color, rofi -combi...

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,13 +2,17 @@ import qualified XMonad.StackSet as W
 import System.IO
 import XMonad
 import XMonad.Actions.CycleWS
+import XMonad.Actions.TagWindows
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.InsertPosition
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.XPropManage
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Spacing
 import XMonad.Layout.Spiral
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.NoFrillsDecoration
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Paste
 import XMonad.Util.Run (spawnPipe)
@@ -22,12 +26,13 @@ myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "Zenity"                       --> doFloat
     , className =? "Steam"                        --> doShift "8"
-    , className =? "Spotify Premium"              --> doShift "7"
+    , className =? "Spotify"                      --> doShift "7"
+    , className =? "Thunar"                       --> doFloat
     , wmRole    =? "gimp-message-dialog"          --> doFloat
     , wmRole    =? "gimp-toolbox-color-dialog"    --> doFloat
     ]
-        where
-            wmRole = stringProperty "WM_WINDOW_ROLE"
+    where
+    wmRole = stringProperty "WM_WINDOW_ROLE"
 
 gaps size = spacingRaw False (Border size 0 size 0) True (Border 0 size 0 size) True
 myLayouts = (gaps 10 $ Tall 1 (3/100) (1/2)) ||| (noBorders Full)
@@ -39,15 +44,16 @@ xK_AudioPlay  = 0x1008FF14
 xK_AudioStop  = 0x1008FF15
 xK_AudioPrev  = 0x1008FF16
 xK_AudioNext  = 0x1008FF17
+xK_prtscrn = 0xff61
                     
 main = do           
     xmproc <- spawnPipe "xmobar"
     xmonad . ewmh . docks $ def
-        { borderWidth        = 3
+        { borderWidth        = 4
         , modMask            = myModKey
-        , focusedBorderColor = "#1343ff"
+        , focusedBorderColor = "#ff4343"
         , terminal           = "kitty"
-        --, focusFollowsMouse  = False
+        , focusFollowsMouse  = False
         --, clickJustFocuses   = False
         , layoutHook         = avoidStruts $ myLayouts
         , manageHook         = namedScratchpadManageHook scratchpads <+> myManageHook <+> manageHook def
@@ -64,14 +70,19 @@ main = do
         , ((0,                      xK_AudioStop),  spawn "playerctl stop")
         , ((0,                      xK_AudioPrev),  spawn "playerctl previous")
         , ((0,                      xK_AudioNext),  spawn "playerctl next")
-        , ((myModKey,               xK_space),      spawn "rofi -show drun")
-        , ((myModKey .|. shiftMask, xK_space),      spawn "rofi -show run")
+        , ((myModKey,               xK_space),      spawn "rofi -matching fuzzy -modi combi -show combi -combi-modi run,drun")
+        --, ((myModKey .|. shiftMask, xK_space),      spawn "rofi -show drun")
+        --, ((myModKey .|. shiftMask, xK_space),      spawn "rofi -show run")
         , ((myModKey,               xK_Tab),        spawn "rofi -show window")
-        , ((myModKey,               xK_l),          spawn "screenlock")
+        , ((0,                       xK_prtscrn),   spawn "gnome-screenshot -a -c")
+        --, ((myModKey,               xK_l),          spawn "screenlock")
+        , ((myModKey .|. shiftMask, xK_F12),        spawn "nitrogen --set-auto --random $HOME/Pictures/wallpapers")
+        , ((myModKey,               xK_a),          sendMessage MirrorShrink)
+        , ((myModKey,               xK_z),          sendMessage MirrorExpand)
         , ((myModKey,               xK_f),          sendMessage NextLayout)
         , ((myModKey,               xK_b),          sendMessage ToggleStruts)
         , ((0,                      xK_F11),        namedScratchpadAction scratchpads "console")
-        , ((0,                      xK_F12),        namedScratchpadAction scratchpads "music")
+        , ((myModKey,               xK_F12),        namedScratchpadAction scratchpads "music")
         ]
 
 scratchpads :: [NamedScratchpad]
